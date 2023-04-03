169434de6d65c958b4cd6f4edd8096dd2a774097
SantosVicente <santos.vicente@aluno.ifsp.edu.br>
2023-04-03 10:30:47 -0300

    new releases

---
+++ scripts/scripts_signup.js
@@ -0,0 +1,25 @@
+const passwordInput = document.getElementById("password");
+const showPasswordBtn = document.getElementById("show-password-btn");
+
+showPasswordBtn.addEventListener("click", function() {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    showPasswordBtn.innerHTML = '<img src="../assets/eye-slash-solid.svg" style="margin-left: -20px;" width="20px" alt="fa fa-eye-slash">';
+  } else {
+    passwordInput.type = "password";
+    showPasswordBtn.innerHTML = '<img src="../assets/eye-solid.svg" width="20px" alt="fa fa-eye">';
+  }
+});
+
+const passwordInput2 = document.getElementById("password_confirm");
+const showPasswordBtn2 = document.getElementById("show-password-btn2");
+
+showPasswordBtn2.addEventListener("click", function() {
+  if (passwordInput2.type === "password") {
+    passwordInput2.type = "text";
+    showPasswordBtn2.innerHTML = '<img src="../assets/eye-slash-solid.svg" style="margin-left: -20px;" width="20px" alt="fa fa-eye-slash">';
+  } else {
+    passwordInput2.type = "password";
+    showPasswordBtn2.innerHTML = '<img src="../assets/eye-solid.svg" width="20px" alt="fa fa-eye">';
+  }
+});
