df80a7ef327f1ef6557c7060533da54d6597c76d
SantosVicente <santos.vicente@aluno.ifsp.edu.br>
2023-04-03 01:31:05 -0300

    forms release and home page final commit

---
+++ scripts/scripts_signin.js
@@ -0,0 +1,12 @@
+const passwordInput = document.getElementById("password");
+const showPasswordBtn = document.getElementById("show-password-btn");
+
+showPasswordBtn.addEventListener("click", function() {
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    showPasswordBtn.innerHTML = '<img src="../assets/eye-slash-solid.svg" style="margin-left: -20px;" width="20px" alt="fa fa-eye-slash">';
+  } else {
+    passwordInput.type = "password";
+    showPasswordBtn.innerHTML = '<img src="../assets/eye-solid.svg" width="20px" alt="fa fa-eye">';
+  }
+});
