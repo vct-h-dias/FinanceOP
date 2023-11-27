7bb0e7d02daa164b8624df4eb0e8d4e7c6723ab8
SantosVicente <santos.vicente@aluno.ifsp.edu.br>
2023-11-27 01:54:10 -0300

    chore: Add CRUD operations for transactions in PHP
    - Implemented POST route for transaction creation.
    - Modified PUT route to allow updating specific fields based on user input.
    - Added DELETE route to remove transactions by transaction_id.
    - Added the transactions table to the database and removed the queries table.

---
+++ php/transactions.php
@@ -0,0 +1,149 @@
+<?php
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Content-type: application/json');
+
+$hostname = "localhost";
+$user = "root";
+$pass = "";
+$database = "FINOP";
+$conn = mysqli_connect($hostname, $user, $pass, $database);
+if (!$conn) {
+  die("Conexão falhou: " . mysqli_connect_error());
+}
+
+$json = file_get_contents('php://input');
+$data = json_decode($json, true);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  if (isset($data['user_id']) && isset($data['description']) && isset($data['value']) && isset($data['month'])) {
+    $user_id = $data['user_id'];
+    $description = $data['description'];
+    $value = $data['value'];
+    $month = $data['month'];
+
+    $query = "INSERT INTO transactions (user_id, description, value, month) VALUES ('$user_id', '$description', '$value', '$month')";
+
+    $res = mysqli_query($conn, $query);
+
+    if ($res) {
+      http_response_code(200);
+      echo json_encode(array(
+        'success' => true,
+        'error' => null
+      ));
+      exit();
+    } else {
+      http_response_code(500);
+      echo json_encode(array(
+        'success' => false,
+        'error' => "Erro ao cadastrar transação"
+      ));
+      exit();
+    }
+  } else {
+    http_response_code(400);
+    echo json_encode(array(
+      'success' => false,
+      'error' => "Campos obrigatórios ausentes"
+    ));
+    exit();
+  }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+  if (isset($data['transaction_id'])) {
+    $transaction_id = $data['transaction_id'];
+    $updateFields = array();
+
+    if (isset($data['description'])) {
+      $description = $data['description'];
+      $updateFields[] = "description='$description'";
+    }
+
+    if (isset($data['value'])) {
+      $value = $data['value'];
+      $updateFields[] = "value='$value'";
+    }
+
+    if (isset($data['month'])) {
+      $month = $data['month'];
+      $updateFields[] = "month='$month'";
+    }
+
+    if (empty($updateFields)) {
+      http_response_code(400);
+      echo json_encode(array(
+        'success' => false,
+        'error' => "Nenhum campo válido fornecido para atualização"
+      ));
+      exit();
+    }
+
+    $updateFieldsString = implode(', ', $updateFields);
+
+    $query = "UPDATE transactions SET $updateFieldsString WHERE id='$transaction_id'";
+
+    $res = mysqli_query($conn, $query);
+
+    if ($res) {
+      http_response_code(200);
+      echo json_encode(array(
+        'success' => true,
+        'error' => null
+      ));
+      exit();
+    } else {
+      http_response_code(500);
+      echo json_encode(array(
+        'success' => false,
+        'error' => "Erro ao atualizar transação"
+      ));
+      exit();
+    }
+  } else {
+    http_response_code(400);
+    echo json_encode(array(
+      'success' => false,
+      'error' => "Campo 'transaction_id' ausente"
+    ));
+    exit();
+  }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+  if (isset($data['transaction_id'])) {
+    $transaction_id = $data['transaction_id'];
+
+    $query = "DELETE FROM transactions WHERE id='$transaction_id'";
+
+    $res = mysqli_query($conn, $query);
+
+    if ($res) {
+      http_response_code(200);
+      echo json_encode(array(
+        'success' => true,
+        'error' => null
+      ));
+      exit();
+    } else {
+      http_response_code(500);
+      echo json_encode(array(
+        'success' => false,
+        'error' => "Erro ao excluir transação"
+      ));
+      exit();
+    }
+  } else {
+    http_response_code(400);
+    echo json_encode(array(
+      'success' => false,
+      'error' => "Campo 'transaction_id' ausente"
+    ));
+    exit();
+  }
+}
